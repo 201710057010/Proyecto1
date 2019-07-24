35c4bcbb52d345ee0d4fafd51bad5abe25888fc5
Eduard <yamiyugi43@gmail.com>
2019-07-23 23:21:28 -0500

    agregado front para el registro de usuario

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,

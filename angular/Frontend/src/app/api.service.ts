0f707c1a4ad1fb0d62670772702648faea473d69
root <root@localhost.localdomain>
2019-08-21 13:30:44 -0500

    version anterior restaurada

--- angular/Frontend/src/app/api.service.ts
+++ angular/Frontend/src/app/api.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  baseurl = 'http://0.0.0.0:8080';
+  baseurl = 'http://127.0.0.1:8080';
 
   httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
